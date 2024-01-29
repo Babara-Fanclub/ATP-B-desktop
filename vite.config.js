d0e122b4bc286c6e77f3b244fb778d6c0bee880e
ecyht2 <ecyht2@nottingham.edu.my>
2024-01-29 15:35:22 +0800

    feat: Added tauri dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 export default {
   root: "./src",
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
   server: {
+    // Tauri expects a fixed port, fail if that port is not available
+    strictPort: true,
     host: "127.0.0.1"
   },
   build: {
     outDir: "../dist",
-    emptyOutDir: true
+    emptyOutDir: true,
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
   }
 }
